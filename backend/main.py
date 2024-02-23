a5d51683403460654a8c0de3474f52da3f31194e
sixzerx <paprut538@gmail.com>
2024-02-23 14:52:54 +0300

    add server

--- backend/main.py
+++ backend/main.py
@@ -1,35 +1,35 @@
-from fastapi import FastAPI, Path
-from typing import Annotated
-from starlette.middleware.cors import CORSMiddleware
-import uvicorn
-from database import Test, db_session
-
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=['*'],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=5500)
-
-
-@app.get('/test1/')
-def test_request():
-    return 'OK'
-
-
-@app.get('/test2/{data}')
-def test_request2(data: Annotated[str, Path()]):
-    return data
-
-
-@app.get('/test_db')
-def test_db():
-    db = db_session()
-    test = db.get(Test, 1)
-    return test
+# from fastapi import FastAPI, Path
+# from typing import Annotated
+# from starlette.middleware.cors import CORSMiddleware
+# import uvicorn
+# from database import Test, db_session
+#
+# app = FastAPI()
+#
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=['*'],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+#
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=5500)
+#
+#
+# @app.get('/test1/')
+# def test_request():
+#     return 'OK'
+#
+#
+# @app.get('/test2/{data}')
+# def test_request2(data: Annotated[str, Path()]):
+#     return data
+#
+#
+# @app.get('/test_db')
+# def test_db():
+#     db = db_session()
+#     test = db.get(Test, 1)
+#     return test
---
+++ server/bot/script.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from server.config import BOT_KEY
+from aiogram.types.web_app_info import WebAppInfo
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=BOT_KEY)
+# Диспетчер
+dp = Dispatcher(bot=bot)
+
+
+# Хэндлер на команду /start
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    website_url = 'https://donorsearch.org/'
+    keyboard = types.ReplyKeyboardMarkup()
+    url_button = types.KeyboardButton(text='s', web_app=WebAppInfo(url=website_url))
+    keyboard.add(url_button)
+    await message.answer("wev'''", reply_markup=keyboard)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
