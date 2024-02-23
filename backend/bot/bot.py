322ddc66c16a4a0f422c445800336b46d53ea115
sixzerx <paprut538@gmail.com>
2024-02-23 17:16:12 +0300

    selectel2

--- backend/bot/bot.py
+++ backend/bot/bot.py
@@ -4,7 +4,7 @@ from aiogram import Bot, Dispatcher, types
 
 from backend.config import BOT_KEY
 from aiogram.types.web_app_info import WebAppInfo
-
+# from aiogram.utils.exceptions
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
 # Объект бота
@@ -16,11 +16,12 @@ dp = Dispatcher(bot=bot)
 # Хэндлер на команду /start
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    website_url = 'https://donorsearch.org/'
-    keyboard = types.ReplyKeyboardMarkup()
-    url_button = types.KeyboardButton(text='s', web_app=WebAppInfo(url=website_url))
+    website_url = 'https://shorturl.at/jstAS'
+    keyboard = types.InlineKeyboardMarkup()
+    url_button = types.InlineKeyboardButton(text='s', web_app=WebAppInfo(url=website_url))
     keyboard.add(url_button)
     await message.answer("wev'''", reply_markup=keyboard)
+
 
 
 async def main():
