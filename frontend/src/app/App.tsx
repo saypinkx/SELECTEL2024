1fa8e86cf97d159e76fade96bfcb4928b9932bcf
Andrey Voronin <AVor0n@yandex.ru>
2024-02-24 04:53:47 +0300

    feat(front): add ssl and telegram integration

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,5 +1,4 @@
 import { view } from '@yoskutik/react-vvm';
-// import { TelegramWebApp } from 'react-telegram-webapp';
 import { AppViewModel } from './AppViewModel';
 import { ThemeProvider, ToasterComponent, ToasterProvider } from '@gravity-ui/uikit';
 import './App.css';
@@ -9,23 +8,17 @@ import Container from 'typedi';
 import { RouterService } from '../services';
 import { RouterProvider } from 'react-router-dom';
 
-// async function validateHash() {
-//     return true;
-// }
-
 export const App = view(AppViewModel)(() => {
     const router = useMemo(() => {
         return Container.get(RouterService).init(routes);
     }, []);
 
     return (
-        // <TelegramWebApp validateHash={validateHash}>
         <ThemeProvider theme="light">
             <ToasterProvider>
                 <RouterProvider router={router} />
                 <ToasterComponent />
             </ToasterProvider>
         </ThemeProvider>
-        // </TelegramWebApp>
     );
 });
